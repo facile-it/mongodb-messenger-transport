001bbb6cb4e2c8a6f01b964d4c806bee65388f6c
Alessandro Lai <alessandro.lai85@gmail.com>
2021-02-19 12:42:00 +0100

    Fix unit tests

--- tests/Unit/Extension/DocumentEnhancer/FirstErrorMessageEnhancerTest.php
+++ tests/Unit/Extension/DocumentEnhancer/FirstErrorMessageEnhancerTest.php
@@ -7,22 +7,36 @@ namespace Facile\MongoDbMessenger\Tests\Unit\Extension\DocumentEnhancer;
 use Facile\MongoDbMessenger\Extension\DocumentEnhancer\FirstErrorMessageEnhancer;
 use MongoDB\Model\BSONDocument;
 use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Stamp\ErrorDetailsStamp;
 use Symfony\Component\Messenger\Stamp\RedeliveryStamp;
 
 class FirstErrorMessageEnhancerTest extends DocumentEnhancerTestCase
 {
     public function testEnhance(): void
     {
+        if (class_exists(ErrorDetailsStamp::class)) {
+            // Symfony 5.2+
+            $stamps = [
+                $stamp = new RedeliveryStamp(456),
+                new ErrorDetailsStamp(\Exception::class, 500, 'Foo Bar'),
+                new RedeliveryStamp(789),
+                new ErrorDetailsStamp(\Exception::class, 500, 'Baz'),
+            ];
+        } else {
+            $stamps = [
+                $stamp = new RedeliveryStamp(456, 'Foo Bar'),
+                new RedeliveryStamp(789, 'Baz'),
+            ];
+        }
+
         $document = new BSONDocument();
-        $stamp = new RedeliveryStamp(456, 'Foo Bar');
-        $stampToBeIgnored = new RedeliveryStamp(789, 'Baz');
         $envelope = new Envelope(new class() {
-        }, [$stamp, $stampToBeIgnored]);
+        }, $stamps);
 
         (new FirstErrorMessageEnhancer())->enhance($document, $envelope);
 
         $this->assertPropertyEquals($stamp->getRedeliveredAt(), $document, 'firstErrorAt');
-        $this->assertPropertyEquals($stamp->getExceptionMessage(), $document, 'firstErrorMessage');
+        $this->assertPropertyEquals('Foo Bar', $document, 'firstErrorMessage');
         $this->assertPropertyDoesNotExist('retryCount', $document);
     }
 
