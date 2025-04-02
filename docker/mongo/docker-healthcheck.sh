2ff73ff3d9199ecc2a402ad8e5dfbb0c917c8edd
Mirko Joshua Mascia <mirko.mascia@facile.it>
2025-04-02 20:25:27 +0200

    config CI to use mongo from docker-compose.yml

--- docker/mongo/docker-healthcheck.sh
+++ docker/mongo/docker-healthcheck.sh
@@ -6,7 +6,7 @@ if [ -z "${MONGO_INITDB_ROOT_USERNAME}" ] || [ -z "${MONGO_INITDB_ROOT_PASSWORD}
 then
 initalize_mongo_rs=$(echo "rs.initiate({_id: \"rs0\", version: 1, members: [{ _id: 0, host : \"127.0.0.1:27017\" }]}).ok || rs.status().ok" | mongo --quiet)
 else
-initalize_mongo_rs=$(echo "rs.initiate({_id: \"rs0\", version: 1, members: [{ _id: 0, host : \"127.0.0.1:27017\" }]}).ok || rs.status().ok" | mongo -u "${MONGO_INITDB_ROOT_USERNAME}" -p "${MONGO_INITDB_ROOT_PASSWORD}" --quiet)
+initalize_mongo_rs=$(echo "rs.initiate({_id: \"rs0\", version: 1, members: [{ _id: 0, host : \"127.0.0.1:27017\" }]}).ok || rs.status().ok" | mongo --port 27017 -u "${MONGO_INITDB_ROOT_USERNAME}" -p "${MONGO_INITDB_ROOT_PASSWORD}" --quiet)
 fi
 
 test $initalize_mongo_rs -eq 1
