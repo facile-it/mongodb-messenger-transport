0a81cbab2617b0587b67ce22dd33f8b6f1aeebe7
Alessandro Lai <alessandro.lai85@gmail.com>
2025-10-12 14:37:33 +0200

    Improve config & serialization validation thanks to PHPStan v2 upgrade

--- src/Transport/Sender.php
+++ src/Transport/Sender.php
@@ -32,8 +32,40 @@ class Sender implements SenderInterface
         $delayStamp = $envelope->last(DelayStamp::class);
         $delay = null !== $delayStamp ? $delayStamp->getDelay() : 0;
 
-        $id = $this->connection->send($envelope, $encodedMessage['body'], $delay, $encodedMessage['headers'] ?? []);
+        if (! array_key_exists('body', $encodedMessage)) {
+            throw new \InvalidArgumentException('Encoded message missing required "body" parameter');
+        }
+
+        if (! is_string($encodedMessage['body'])) {
+            throw new \InvalidArgumentException('Encoded message must be a string, got ' . gettype($encodedMessage['body']));
+        }
+
+        $encodedMessage['headers'] ??= [];
+        if (! is_array($encodedMessage['headers'])) {
+            throw new \InvalidArgumentException('Encoded headers must be an array, got ' . gettype($encodedMessage['headers']));
+        }
+        $this->assertContainsOnlyStrings($encodedMessage['headers']);
+
+        $id = $this->connection->send($envelope, $encodedMessage['body'], $delay, $encodedMessage['headers']);
 
         return $envelope->with(new TransportMessageIdStamp($id));
+    }
+
+    /**
+     * @param mixed[] $headers
+     *
+     * @phpstan-assert array<string, string> $headers
+     */
+    private function assertContainsOnlyStrings(array $headers): void
+    {
+        foreach ($headers as $key => $header) {
+            if (! is_string($key)) {
+                throw new \InvalidArgumentException('Encoded message headers must have string keys, got ' . gettype($key));
+            }
+
+            if (! is_string($header)) {
+                throw new \InvalidArgumentException('Encoded message headers must be strings, got ' . gettype($header));
+            }
+        }
     }
 }
